b1631a7f619d4c040a965c19c36c5190816cc3a7
Maisa Batista Costa <b14maisabatistacostasantos@gmail.com>
2023-01-04 22:44:39 -0300

    Primal

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import './questionario.dart';
+import './resultado.dart';
+
+main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual è a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual è a seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual è o seu instrutor favorito?',
+      'respostas': ['Maria', 'Joao', 'Leo', 'Pedro'],
+    }
+  ];
+
+  void _responder() {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+    print(_perguntaSelecionada);
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //for (String textoResp
+    //  in respostas) {
+    //widgets.add(Resposta(textoResp, _respoder));
+    //}
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder)
+            : Resultado(),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
